5ac79b7368148f20206c3ea8ed2186cf81f9960b
Nicolas Straub <spameggspambakedbeansandspam@gmail.com>
2015-03-11 15:26:58 -0300

    IE 11 doesn't play nicely with karma. disabling IE for now

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome', 'Firefox', 'PhantomJS', 'IE'],
+        browsers: ['Chrome', 'Firefox', 'PhantomJS'],
 
 
         // Continuous Integration mode
