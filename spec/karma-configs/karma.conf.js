c71681c1374a47bf656a98f5e52747f980c32ddd
Nicolas Straub <spameggspambakedbeansandspam@gmail.com>
2017-08-22 11:47:32 -0300

    simplifying karma config

--- spec/karma-configs/karma.conf.js
+++ spec/karma-configs/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
 
           // list of files / patterns to load in the browser
           files: [
-              {pattern: './spec/**/*.js', watched: false},
+              {pattern: './spec/**/runner.js', watched: false},
           ],
 
 
@@ -30,7 +30,7 @@ module.exports = function (config) {
           // preprocess matching files before serving them to the browser
           // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
           preprocessors: {
-              './spec/**/*.js': ['webpack', 'sourcemap']
+              './spec/**/runner.js': ['webpack', 'sourcemap']
           },
           babelPreprocessor: {
               options: {
